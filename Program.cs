0cd0566a5df6cc9bb737700df530d04f35158d07
dmitriy <boss.vede@mail.ru>
2022-08-18 16:36:24 +0700

    third exercise

--- Program.cs
+++ Program.cs
@@ -33,3 +33,11 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+// System.Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Кубы чисел от 1 до {number}");
+// for (int i = 1; i <= number; i++)
+// {
+//     System.Console.Write($"{Math.Pow(i, 3)} ");
+// }
